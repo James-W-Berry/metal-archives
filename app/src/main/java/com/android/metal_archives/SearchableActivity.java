dd58511cd6ac84bec2dbf537f95cafd303c82f3c
James-W-Berry <james.berry.iv@gmail.com>
2017-08-23 10:42:08 -0400

    search result styling

--- app/src/main/java/com/android/metal_archives/SearchableActivity.java
+++ app/src/main/java/com/android/metal_archives/SearchableActivity.java
@@ -1,5 +1,6 @@
 package com.android.metal_archives;
 
+import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.renderscript.ScriptGroup;
@@ -12,7 +13,9 @@ import android.view.KeyEvent;
 import android.view.View;
 import android.view.WindowManager;
 import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+import android.widget.TableLayout;
 import android.widget.TextView;
 
 import org.jsoup.Jsoup;
@@ -31,6 +34,7 @@ import java.io.StringReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Iterator;
 
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
@@ -45,13 +49,19 @@ import javax.xml.xpath.XPathFactory;
 
 public class SearchableActivity extends AppCompatActivity {
     private EditText search;
-    private TextView content_view;
+    private TextView search_prep;
+    private TableLayout band_tile;
+    private String country_in;
+    private String location_in;
+    private String status_in;
+    private String formed_in;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
-        content_view = (TextView) findViewById(R.id.test_data);
+        search_prep = (TextView) findViewById(R.id.search_prep);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle("");
@@ -63,10 +73,9 @@ public class SearchableActivity extends AppCompatActivity {
             getSupportActionBar().setDisplayShowHomeEnabled(true);
         }
 
-        // open keyboard for search
         search = (EditText) findViewById(R.id.search_edit);
         search.setVisibility(View.VISIBLE);
-        search.requestFocus();
+        search.requestFocus(); // open keyboard for search
         if(search.requestFocus()) {
             getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
         }
@@ -76,6 +85,9 @@ public class SearchableActivity extends AppCompatActivity {
             @Override
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
                 if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                    search.clearFocus(); // close keyboard
+                    InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(search.getWindowToken(), 0);
                     getWebsite(search.getText().toString());
                     return true;
                 }
@@ -93,22 +105,60 @@ public class SearchableActivity extends AppCompatActivity {
 
     private void getWebsite(String band) {
         Log.i("SeachableActivity", "searching for " + band);
+        final String band_name = band;
         new Thread(new Runnable() {
             @Override
             public void run() {
                 final StringBuilder builder = new StringBuilder();
 
                 try {
-                    Document doc = Jsoup.connect("https://www.metal-archives.com/bands/Gojira").get();
+                    Document doc = Jsoup.connect("https://www.metal-archives.com/bands/" + band_name).get();
                     String title = doc.title();
-                    Elements links = doc.select("a[href]");
 
-                    builder.append(title).append("\n");
+                    builder.append(band_name).append("\n\n");
 
-                    for (Element link : links) {
-                        builder.append("\n").append("Link : ").append(link.attr("href"))
-                                .append("\n").append("Text : ").append(link.text());
+                    Elements band_stats = doc.select("div#band_stats");
+                    for (Element spec : band_stats) {
+                        Elements dl = spec.select("dl.float_left");
+                        Elements dts = dl.select("dt");
+                        Elements dds = dl.select("dd");
+
+                        Iterator<Element> dtsIterator = dts.iterator();
+                        Iterator<Element> ddsIterator = dds.iterator();
+
+                        while (dtsIterator.hasNext() && ddsIterator.hasNext()) {
+                            Element dt = (Element) dtsIterator.next();
+                            Element dd = (Element) ddsIterator.next();
+
+                            switch (dt.text()){
+                                case "Country of origin:":
+                                    country_in = dt.text() + " " + dd.text().replaceAll("\\s{2,}", " ").trim();
+                                    break;
+                                case "Location:":
+                                    location_in = dt.text() + " " + dd.text().replaceAll("\\s{2,}", " ").trim();
+                                    break;
+                                case "Status:":
+                                    status_in = dt.text() + " " + dd.text().replaceAll("\\s{2,}", " ").trim();
+                                    break;
+                                case "Formed in:":
+                                    formed_in = dt.text() + " " + dd.text().replaceAll("\\s{2,}", " ").trim();
+                                    break;
+                                default:
+                                    break;
+                            }
+
+                            builder.append(dt.text() + " " + dd.text() + "\n");
+                            System.out.println(dt.text() + dd.text());
+                        }
                     }
+                    //builder.append(band_stats.text());
+
+//                    Elements links = doc.select("a[href]");
+//                    for (Element link : links) {
+//                        builder.append("\n").append("Link : ").append(link.attr("href"))
+//                                .append("\n").append("Text : ").append(link.text());
+//                    }
+
                 } catch (IOException e) {
                     builder.append("Error : ").append(e.getMessage()).append("\n");
                 }
@@ -116,7 +166,19 @@ public class SearchableActivity extends AppCompatActivity {
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        content_view.setText(builder.toString());
+                        search_prep.setVisibility(View.GONE);
+                        band_tile = (TableLayout) findViewById(R.id.band_tile);
+                        band_tile.setVisibility(View.VISIBLE);
+                        TextView name = (TextView) findViewById(R.id.band_name);
+                        name.setText(band_name);
+                        TextView country = (TextView) findViewById(R.id.band_country);
+                        country.setText(country_in);
+                        TextView location = (TextView) findViewById(R.id.band_location);
+                        location.setText(location_in);
+                        TextView status = (TextView) findViewById(R.id.band_status);
+                        status.setText(status_in);
+                        TextView formed = (TextView) findViewById(R.id.band_formed);
+                        formed.setText(formed_in);
                     }
                 });
             }
